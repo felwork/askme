1d2a16d0dfeac0aa28cd4ff1271086edad9c181f
felwork <alexandr.mityushkin94@gmail.com>
2022-02-17 12:39:28 +0300

    added index on user and downcase email in method authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,14 +15,14 @@ class User < ApplicationRecord
   validates :password, confirmation: true
 
   before_save :encrypt_password
-  before_validation :downcase_name_and_email, on: :create
+  before_validation :downcase_name_and_email
 
   def self.hash_to_string(password_hash)
     password_hash.unpack('H*')[0]
   end
 
   def self.authenticate(email, password)
-    user = find_by(email: email)
+    user = find_by(email: email.downcase)
 
     user if user.present? && user.password_hash == User.hash_to_string(
       OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_14_130849) do
+ActiveRecord::Schema.define(version: 2022_02_16_143742) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2022_02_14_130849) do
     t.string "password_hash"
     t.string "password_salt"
     t.string "avatar_url"
+    t.index ["username", "email"], name: "index_users_on_username_and_email", unique: true
   end
 
 end
